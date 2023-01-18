8ae36b427845a4cb64f1238b04a32fa403f45389
Rodrigo José <rodrigo.angelis@grupoeureka.com.br>
2023-01-18 10:54:55 -0300

    assets and splash

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Tamu Aqui!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ff0000',
   },
 });
