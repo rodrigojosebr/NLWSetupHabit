1f408e87ef240019ad852d80862cb32ad12f99c7
Rodrigo José <rodrigo.angelis@grupoeureka.com.br>
2023-01-18 12:04:19 -0300

    fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,27 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold, useFonts
+} from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fonstsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fonstsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Tamu Aqui!</Text>
@@ -13,12 +33,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     fontSize: 20,
+    fontFamily: 'Inter_700Bold',
     fontWeight: 'bold',
     color: '#ff0000',
   },
